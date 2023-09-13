e41aca37350833a77829c063c90c8c1d645ef624
whitecrow <edin.whitecrow@gmail.com>
2023-09-13 21:44:21 +0900

    푸드 파이터 정답 추가

--- 9/230913#.푸드 파이트 대회.py
+++ 9/230913#.푸드 파이트 대회.py
@@ -39,3 +39,31 @@ def solution(food):
 #     answer.extend(answer[:-1][::-1])
 #     # join을 사용해 문자열을 하나로 합침
 #     return ''.join(answer)
+
+
+# def solution(food):
+#     answer = ''
+#     # answer의 중간지점 index 번호
+#     mid_idx = 0
+#     for i in range(1,len(food)):
+#         # 한쪽에 채워야할 i번 음식 개수
+#         num_food = food[i]//2
+#         # answer의 중간 지점에 "i"를 i 음식 개수의 2배를 넣는다. (양쪽 집어넣어야하니깐)
+#         answer = answer[:mid_idx] + str(i) * num_food * 2 + answer[mid_idx:]
+#         # 중간지점 index 갱신
+#         mid_idx += num_food
+#     # 마지막으로 물 넣기
+#     answer = answer[:mid_idx] + "0" + answer[mid_idx:]
+
+#     return answer
+
+
+# def solution(food):
+#     answer = ''
+#     for i in range(1,len(food)):
+#         answer += str(i) * (food[i]//2)
+
+#     answer_reverse = answer[::-1]
+#     answer = answer + '0' + answer_reverse
+
+#     return answer
