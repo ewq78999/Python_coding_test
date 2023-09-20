e2b04253d19671a4c3e2ee6faca744813096e329
whitecrow <edin.whitecrow@gmail.com>
2023-09-20 11:22:46 +0900

    크기가 작은 부분문자열

--- 9/230920.크기가 작은 부분문자열.py
+++ 9/230920.크기가 작은 부분문자열.py
@@ -1,11 +1,12 @@
 def solution(t, p):
     answer = 0
-    P = int(p)
-    T = len(t)
-    
-    for i in range(T - len(p) + 1):
+    l = len(t)
+    # t에서 p와 길이가 같은 부분문자열 추출 필요
+    # 만약 남은 t의 길이가 p의 길이보다 짧다면 추출할 수 없으므로 len(p)를 뺴고 range니까 종료 인덱스 +1
+    for i in range(l - len(p) + 1):
+        # 현재 글자 "" 상태니까 숫자 비교하려면 int() 사용해야 함
         a = int(t[i:i+len(p)])
-        if a <= P:
+        if a <= int(p):
             answer += 1
             
     return answer
@@ -22,3 +23,4 @@ def solution(t, p):
 #             if int(a) <= P:
 #                 answer += 1
 #     return answer
+                
