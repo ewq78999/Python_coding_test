e06f21eb9b2b58d51f13ee5813b4ca8fccaec54c
whitecrow <edin.whitecrow@gmail.com>
2023-09-30 23:08:23 +0900

    포켓몬_min

--- 9/230930.폰켓몬.py
+++ 9/230930.폰켓몬.py
@@ -3,7 +3,7 @@
 def solution(nums):
     result = 0
     amount = len(nums)//2
-    # set을 사용하여 복수값 제거해라
+    # set을 사용하여 중복값 제거해라
     n = set(nums)
     
     # 만일 가질 수 있는 최대값인 n/2 보다 set()을 사용했을 때 종류가 작다면...set()한 종류밖에 선택 할 수 없다
@@ -14,3 +14,6 @@ def solution(nums):
         result = amount
         
     return result
+
+def solution(nums):
+    return min(len(nums) // 2, len(set(nums)))
