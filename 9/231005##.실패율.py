da49b32c6ce52239f8d4be82cabd4d714575a9ee
whitecrow <edin.whitecrow@gmail.com>
2023-10-06 01:48:15 +0900

    실패율

--- 9/231005##.실패율.py
+++ 9/231005##.실패율.py
@@ -1,23 +1,28 @@
-# N : 단계 수
-# stages: 사람들의: 현재 단계
-# result: 가장 많이 실패한 스테이지 순으로 내림차순 나열
 def solution(N, stages):
-    answer = []
+    # 딕셔너리 생성
+    fail_per = {}
+    answer = 0
+    people = len(stages)
+    
+    for i in range(1, N+1):
+        # count를 통해 i의 갯수(빈도)를 반환, ex) 1은 1개 있으므로 1 반환, 2는 3개 있으므로 3 반환
+        fail = stages.count(i)
+        # 다 떨어지고 도달한 유저가 없으면
+        if people == 0:
+            # 실패율을 0으로 정의
+            fail_per[i] = 0
+        else:
+            # i : fail/people 로 키 : 값을 할당
+            fail_per[i] = fail / people
+            people -= fail
+            
+    # .get으로 반환된 fail_per값을 key로 해서 sorted한다, 이후 내림차순
+    answer = sorted(fail_per, key=fail_per.get, reverse=True)
+    
+    return answer
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     
 #     level = []
 #     fail1 = 0
