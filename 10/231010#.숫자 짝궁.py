5a8314cb6a45e0eee6a343d7815b4a8230a7aa51
whitecrow <edin.whitecrow@gmail.com>
2023-10-10 15:12:35 +0900

    오답 수정 및 새 정답

--- 10/231010#.숫자 짝궁.py
+++ 10/231010#.숫자 짝궁.py
@@ -1,15 +1,38 @@
+#     # 오답
+#     answer = ''
+#     Y = list(Y)
+    
+#     for i in X:
+#         if i in Y:
+#             answer += i
+#             Y.remove(i)
+            
+#     answer = ''.join(sorted(answer, reverse=True))
+    
+#     # {}를 사용해 집합 표현, '0'만을 포함하는 집합인지 확인
+#     if set(answer) == {'0'}:
+#         answer = '0'
+    
+#     if len(answer) == 0:
+#         answer = '-1'
+    
+#     return answer
+
 def solution(X, Y):
-    answer = ''
+    answer = []
+    # 중복값을 없앤 것들의 교집합
+    for i in (set(X) & set(Y)):
+        # 같은 수가 count(i) -1 까지 반복되서 추가되게
+        # 그러니까 만약 5가 X에 3개 있어도 Y에 2개만 있기에 min(2)만큼만 추가되게 함
+        for j in range(min(X.count(i), Y.count(i))):
+            answer.append(i)
+            
+    answer.sort(reverse=True)
     
-    for i in X:
-        if i in Y:
-            answer += i
+    if set(answer) == {'0'}:
+        answer = '0'
     
-    A = set(answer)
-    answer = sorted(A, reverse=True)
-    a = ''.join(answer)
-    
-    if not len(a):
-        a = '-1'
-    
-    return a
+    if len(answer) == 0:
+        answer = '-1'
+        
+    return ''.join(answer)
